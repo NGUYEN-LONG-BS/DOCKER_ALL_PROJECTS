38c2752256248047e10bb44ccd6b2b6c000c22cd
NGUYEN-LONG-BS <nguyenlongbs2012@gmail.com>
2025-04-12 15:41:57 +0700

    lưu

--- django-docker-TAG/tuan_an_group/tuan_an_group/settings.py
+++ django-docker-TAG/tuan_an_group/tuan_an_group/settings.py
@@ -117,9 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-# STATIC_URL = 'static/'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']  # Path to your static files directory
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',    # Path to your static files directory
+    ]  
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # Directory where static files will be collected
 
 # Default primary key field type
