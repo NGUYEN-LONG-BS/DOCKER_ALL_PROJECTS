db47b2a1792a6ba6ca4050b2dc5bec0212c669aa
NGUYEN-LONG-BS <nguyenlongbs2012@gmail.com>
2025-04-17 17:03:28 +0700

    commit

--- my-demo-project-01/danh_muc_san_pham/admin.py
+++ my-demo-project-01/danh_muc_san_pham/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Product, Category
+from import_export import resources
+from import_export.admin import ExportMixin
 
-admin.site.register(Product)
+# Tạo resource cho Product
+class ProductResource(resources.ModelResource):
+    class Meta:
+        model = Product
+
+class ProductAdmin(ExportMixin, admin.ModelAdmin):
+    resource_class = ProductResource
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Category)
+
