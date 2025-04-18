34ae62561e2439583ceb27f7878925e79ef38608
NGUYEN-LONG-BS <nguyenlongbs2012@gmail.com>
2025-04-18 09:24:45 +0700

    hoàn thành app dụng bootstrap 5 vào dự án Front End

--- pro_01_next_django/FrontEnd/my-next-app/src/app/layout.tsx
+++ pro_01_next_django/FrontEnd/my-next-app/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "bootstrap/dist/css/bootstrap.min.css"; // Nhập Bootstrap 5
+import "./globals.css"; // Nếu bạn có thêm CSS tùy chỉnh
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
