f2c429409a6c1e134b4b937038120f6f3c23b5dd
NGUYEN-LONG-BS <nguyenlongbs2012@gmail.com>
2025-04-23 10:54:19 +0700

    fix all errors

--- pro_01_next_django/FrontEnd/my-next-app/src/app/inventory-management/layout.tsx
+++ pro_01_next_django/FrontEnd/my-next-app/src/app/inventory-management/layout.tsx
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "Quản Lý Danh Mục Hàng Hoá",
   description: "Hệ thống quản lý danh mục hàng hoá",
-    generator: 'v0.dev'
+  generator: 'LongNguyen'
 }
 
 export default function RootLayout({
@@ -18,24 +18,40 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="vi">
-      <head>
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-          crossOrigin="anonymous"
-        />
-      </head>
-      <body className={inter.className}>
-        {children}
-        <Script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-          crossOrigin="anonymous"
-          strategy="afterInteractive"
-        />
-      </body>
-    </html>
+    // <html lang="vi">
+    //   <head>
+    //     <link
+    //       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+    //       rel="stylesheet"
+    //       integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+    //       crossOrigin="anonymous"
+    //     />
+    //   </head>
+    //   <body className={inter.className}>
+    //     {children}
+    //     <Script
+    //       src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    //       integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+    //       crossOrigin="anonymous"
+    //       strategy="afterInteractive"
+    //     />
+    //   </body>
+    // </html>
+
+
+
+      <div className={inter.className}>
+      {/* Nội dung của inventory-management */}
+      {children}
+
+      {/* Script bổ sung cho Bootstrap */}
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossOrigin="anonymous"
+        strategy="afterInteractive"
+      />
+      </div>
+
   )
 }
