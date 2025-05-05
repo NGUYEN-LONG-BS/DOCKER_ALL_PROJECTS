6f7b79e1a8e3e66925b6b4739ccb66795008cd80
NGUYEN-LONG-BS <nguyenlongbs2012@gmail.com>
2025-05-05 17:01:34 +0700

    add post into tab 3

--- pro_01_next_django/FrontEnd/my-next-app/src/components/inventoryManagement/inventory_category_post.tsx
+++ pro_01_next_django/FrontEnd/my-next-app/src/components/inventoryManagement/inventory_category_post.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Định nghĩa kiểu dữ liệu cho form
 interface InventoryFormData {
