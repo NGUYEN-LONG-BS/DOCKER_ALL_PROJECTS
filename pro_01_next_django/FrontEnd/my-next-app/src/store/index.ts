0e8e361db2994855ecdaaba903a261c84d1ce312
NGUYEN-LONG-BS <nguyenlongbs2012@gmail.com>
2025-05-08 16:22:53 +0700

    config redux toolKit

--- pro_01_next_django/FrontEnd/my-next-app/src/store/index.ts
+++ pro_01_next_django/FrontEnd/my-next-app/src/store/index.ts
@@ -1,11 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import inventoryReducer from './inventorySlice';
+import myReducer from './slices/mySlice'; // Adjust path if needed
 
 export const store = configureStore({
   reducer: {
     inventory: inventoryReducer,
+    myFeature: myReducer, // For mySlice
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
