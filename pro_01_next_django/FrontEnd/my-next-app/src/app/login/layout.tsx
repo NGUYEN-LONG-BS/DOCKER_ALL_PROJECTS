f037f02277ae671069ce98e4f6940dc5c0a41f37
NGUYEN-LONG-BS <nguyenlongbs2012@gmail.com>
2025-05-21 10:43:52 +0700

    modify code

--- pro_01_next_django/FrontEnd/my-next-app/src/app/login/layout.tsx
+++ pro_01_next_django/FrontEnd/my-next-app/src/app/login/layout.tsx
@@ -2,14 +2,14 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
-import "bootstrap/dist/css/bootstrap.min.css"
+// import "bootstrap/dist/css/bootstrap.min.css"
 
+// Khởi tạo font Inter với subsets latin
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Login/Subscribe Page",
   description: "A beautiful login and subscribe interface",
-    generator: 'v0.dev'
 }
 
 export default function RootLayout({
@@ -19,6 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      {/* Áp dụng className từ font Inter */}
       <body className={inter.className}>{children}</body>
     </html>
   )
