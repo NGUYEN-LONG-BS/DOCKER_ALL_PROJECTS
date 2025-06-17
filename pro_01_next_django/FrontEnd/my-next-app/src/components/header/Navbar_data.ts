edc9c548c123efcf1d4fb63b4adc4acb51128708
NGUYEN-LONG-BS <nguyenlongbs2012@gmail.com>
2025-06-17 16:52:25 +0700

    clean code

--- pro_01_next_django/FrontEnd/my-next-app/src/components/header/Navbar_data.ts
+++ pro_01_next_django/FrontEnd/my-next-app/src/components/header/Navbar_data.ts
@@ -62,50 +62,6 @@ export const menuItems: MenuItem[] = [
         ],
       },
       {
-        label: 'Django',
-        permission: ['Admin'],
-        submenu_02: [
-          { label: 'Docker 01', href: '/html/django/start_dijango_with_docker_step_01.html' },
-          { label: 'Docker 02', href: '/html/django/start_dijango_with_docker_step_02.html' },
-          { label: 'Docker 03', href: '/html/django/start_dijango_with_docker_step_03.html' },
-          { label: 'Docker 04', href: '/html/django/start_dijango_with_docker_step_04.html' },
-        ],
-      },
-      {
-        label: 'nextjs',
-        permission: ['Admin'],
-        submenu_02: [
-          { label: 'flow-redux-toolkit', href: '/html/nextjs/flow-redux-toolkit.html' },
-          { label: 'next-link-to-html-static', href: '/html/nextjs/next-link-to-html-static.html' },
-          { label: 'react-hooks', href: '/html/nextjs/react-hooks.html' },
-          { label: 'react-redux', href: '/html/nextjs/react-redux.html' },
-          { label: 'redux-toolkit', href: '/html/nextjs/redux-toolkit.html' },
-          { label: 'useState-useReducer', href: '/html/nextjs/useState-useReducer.html' },
-        ],
-      },
-      {
-        label: 'economic',
-        permission: ['Admin'],
-        submenu_02: [
-          { label: 'Lãi suất Việt Nam', href: '/html/economic/eco-interest_rates_full.html' },
-        ],
-      },
-      {
-        label: 'orther-projects',
-        permission: ['Admin'],
-        submenu_02: [
-          { label: 'base', href: '/html/orther-projects/base.html' },
-          { label: 'danh_muc_san_pham', href: '/html/orther-projects/danh_muc_san_pham.html' },
-          { label: 'home', href: '/html/orther-projects/home.html' },
-          { label: 'index', href: '/html/orther-projects/index.html' },
-          { label: 'login', href: '/html/orther-projects/login.html' },
-          { label: 'mychart', href: '/html/orther-projects/mychart.html' },
-          { label: 'ngay_quan_trong', href: '/html/orther-projects/ngay_quan_trong.html' },
-          { label: 'postGreSQL-config-remote-connecttion', href: '/html/orther-projects/postGreSQL-config-remote-connecttion.html' },
-          { label: 'user', href: '/html/orther-projects/user.html' },
-        ],
-      },
-      {
         label: 'Test link',
         permission: ['Admin'],
         submenu_02: [
