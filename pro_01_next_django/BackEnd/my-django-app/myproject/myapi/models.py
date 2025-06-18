1cd6b2596ea9d8ac3df0b17e55177ad597fdb9da
NGUYEN-LONG-BS <nguyenlongbs2012@gmail.com>
2025-06-18 09:24:13 +0700

    adding 2 more models for client and supplier table

--- pro_01_next_django/BackEnd/my-django-app/myproject/myapi/models.py
+++ pro_01_next_django/BackEnd/my-django-app/myproject/myapi/models.py
@@ -98,3 +98,43 @@ class UserPermission(models.Model):
     class Meta:
         db_table = 'inventory"."user_permissions'  # Đúng tên bảng trong PostgreSQL
         managed = True  # Django sẽ quản lý bảng này hoàn toàn (tạo, xóa, sửa theo model).
+
+class TB_CLIENT_CATEGORIES(models.Model):
+    id = models.UUIDField(primary_key=True)
+    date = models.DateTimeField()
+    id_nhan_vien = models.CharField(max_length=10)
+    xoa_sua = models.CharField(max_length=10)
+    ma_khach_hang = models.CharField(max_length=50)
+    ten_khach_hang = models.TextField()
+    ma_phan_loai_01 = models.CharField(max_length=50)
+    ma_phan_loai_02 = models.CharField(max_length=50)
+    ma_phan_loai_03 = models.CharField(max_length=50)
+    ma_phan_loai_04 = models.CharField(max_length=50)
+    ma_phan_loai_05 = models.CharField(max_length=50)
+    ma_phan_loai_06 = models.CharField(max_length=50)
+    ma_phan_loai_07 = models.CharField(max_length=50)
+    ma_phan_loai_08 = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'inventory.TB_CLIENT_CATEGORIES'
+        managed = True  # Django sẽ quản lý bảng này hoàn toàn (tạo, xóa, sửa theo model).
+        
+class TB_SUPPLIER_CATEGORIES(models.Model):
+    id = models.UUIDField(primary_key=True)
+    date = models.DateTimeField()
+    id_nhan_vien = models.CharField(max_length=10)
+    xoa_sua = models.CharField(max_length=10)
+    ma_nha_cung_cap = models.CharField(max_length=50)
+    ten_nha_cung_cap = models.TextField()
+    ma_phan_loai_01 = models.CharField(max_length=50)
+    ma_phan_loai_02 = models.CharField(max_length=50)
+    ma_phan_loai_03 = models.CharField(max_length=50)
+    ma_phan_loai_04 = models.CharField(max_length=50)
+    ma_phan_loai_05 = models.CharField(max_length=50)
+    ma_phan_loai_06 = models.CharField(max_length=50)
+    ma_phan_loai_07 = models.CharField(max_length=50)
+    ma_phan_loai_08 = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'inventory.TB_SUPPLIER_CATEGORIES'
+        managed = True  # Django sẽ quản lý bảng này hoàn toàn (tạo, xóa, sửa theo model).
