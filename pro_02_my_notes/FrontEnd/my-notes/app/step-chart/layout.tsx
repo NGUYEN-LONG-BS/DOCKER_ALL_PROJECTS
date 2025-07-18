db47feb044380cf564bdfeaa520fe8535765af94
NGUYEN-LONG-BS <nguyenlongbs2012@gmail.com>
2025-07-18 10:40:54 +0700

    fix bug in layout.tsx

--- pro_02_my_notes/FrontEnd/my-notes/app/step-chart/layout.tsx
+++ pro_02_my_notes/FrontEnd/my-notes/app/step-chart/layout.tsx
@@ -13,19 +13,5 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  return (
-    <html lang="vi">
-      <head>
-        <meta charSet="UTF-8" />
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
-          crossOrigin="anonymous"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </head>
-      <body>{children}</body>
-    </html>
-  )
+  return <>{children}</>;
 }
